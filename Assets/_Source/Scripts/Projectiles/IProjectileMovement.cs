97777b287cf030164119a79dfca30a64364e6cbd
PlotvaIzLodzya <gordoy92@gmail.com>
2025-08-17 20:46:51 +0300

    Rename namespace based on folder path

--- Assets/_Source/Scripts/Projectiles/IProjectileMovement.cs
+++ Assets/_Source/Scripts/Projectiles/IProjectileMovement.cs
@@ -1,7 +1,7 @@
 ﻿using _Source.Scripts.Battle;
 using System.Collections;
 
-namespace _Source.Scripts.Buildings
+namespace _Source.Scripts.Projectiles
 {
     public interface IProjectileMovement
     {

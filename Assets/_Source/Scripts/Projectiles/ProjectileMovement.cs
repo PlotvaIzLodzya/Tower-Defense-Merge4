cb215a38eb570e546f8077826f684c04b1d61adf
PlotvaIzLodzya <gordoy92@gmail.com>
2025-08-19 19:14:16 +0300

    Move projectile life time to base class

--- Assets/_Source/Scripts/Projectiles/ProjectileMovement.cs
+++ Assets/_Source/Scripts/Projectiles/ProjectileMovement.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 namespace _Source.Scripts.Projectiles
 {
+
     public abstract class ProjectileMovement : ScriptableObject, IProjectileMovement
     {
+        [field: SerializeField] public float LifeTime { get; private set; }
+
         public abstract IEnumerator Moving(Enemy enemy, Projectile projectile);
     }
 }
--- Assets/_Source/Scripts/Projectiles/StraightMovement.cs
+++ Assets/_Source/Scripts/Projectiles/StraightMovement.cs
@@ -7,17 +7,16 @@ namespace _Source.Scripts.Projectiles
 {
     [CreateAssetMenu(fileName = nameof(StraightMovement), menuName = NamingConstant.ProjectilesBehaviour + "/" + NamingConstant.ProjectilesMovement + "/" + nameof(StraightMovement))]
     public class StraightMovement : ProjectileMovement
-    {
-        [SerializeField] private float _lifeTime;
-       
+    {       
         public override IEnumerator Moving(Enemy enemy, Projectile projectile)
         {
             var elapsedTime = 0f;
 
             var direction = (enemy.transform.position - projectile.transform.position).normalized;
 
-            while(elapsedTime < _lifeTime)
+            while(elapsedTime < LifeTime)
             {
+                elapsedTime += Time.deltaTime;
                 projectile.transform.position += direction * projectile.Stats.Speed * Time.deltaTime;
                 yield return null;
             }

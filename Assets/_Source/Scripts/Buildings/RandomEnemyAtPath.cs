ad9d0e9ba4c2fab5c784a6ca7d2f4330f4407e6b
PlotvaIzLodzya <gordoy92@gmail.com>
2025-08-19 19:15:11 +0300

    Add random target enemy seek

---
+++ Assets/_Source/Scripts/Buildings/RandomEnemyAtPath.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using _Source.Scripts.Battle;
+using UnityEngine;
+
+namespace _Source.Scripts.AttackBehaviours
+{
+    public class RandomEnemyAtPath : ITargetSeek
+    {
+        private EnemyPool _enemiesPool;
+
+        public RandomEnemyAtPath(EnemyPool enemiesPool)
+        {
+            _enemiesPool = enemiesPool;
+        }
+
+        public bool TryGetTarget(out Enemy enemy)
+        {
+            enemy = _enemiesPool.GetEnemy(GetRandomEnemy);
+
+            return enemy != null;
+        }
+
+        private Enemy GetRandomEnemy(List<Enemy> enemies)
+        {
+            var randomIndex = Random.Range(0, enemies.Count);
+            return enemies[randomIndex];
+        }
+    }
+}
